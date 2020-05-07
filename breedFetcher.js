e4e3356d851b3e8c5c755c1d9a762d7af152ef96
Emily <emilymnicholas@gmail.com>
2020-05-07 22:39:02 +0000

    linted :)

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,16 @@
-const request = require('request')
+const request = require('request');
 
+const breed = (process.argv.slice(2));
 
-let breed = (process.argv.slice(2))
-//console.log("breed", breed)
-
-request('https://api.thecatapi.com/v1/breeds/search?q='+ breed, (error, response, body) => {
+request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
   if (error) {
-    console.log('this is an error: ', error)
+    console.log('ERROR OCCURED: ', error);
   } else if (body.length === 2) {
-    console.log('Error, cat breed not found', error); // Print the error if one occurred
-      }
-  const data = JSON.parse(body);
-  console.log(data[0]['description'])
+    console.log('ERROR: cat breed not found');
+  } else {
+    const data = JSON.parse(body);
+    console.log(data[0]['description']);
+  }
 });
-
 
 
